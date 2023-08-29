656647aedb6fde364bf66ebe8b43ec64ccf11482
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-08-29 15:08:39 +0200

    i used js  operators

--- js_activite/lesson2/operator.js
+++ js_activite/lesson2/operator.js
@@ -18,3 +18,25 @@ let secondOne= a-=4;
 console.log(firstOne*secondOne)
 let total = firstOne * secondOne ;
 document.getElementById("myOperator").innerHTML = total;
+
+let text1 = "bill"
+let text2 = "takes"
+let text3 = text1 + " " + text2 ;
+console.log(text3);
+text3 += "35ans"
+console.log(text3);
+//
+let x = 5;
+x++;
+console.log(x);
+let y = 4;
+y--;
+console.log(y);
+let z = 6;
+//exponentiation
+let t = z**3;
+console.log(t);
+
+let d = 4;
+let Myresult = Math.pow(d,4);
+console.log(Myresult);
