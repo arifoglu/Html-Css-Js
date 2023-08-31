7733bb26373ef30a1a89938247a1c3ede8957947
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-08-31 11:28:37 +0200

    i used js strings methods

---
+++ js_activite/lesson3/strings.js
@@ -0,0 +1,12 @@
+let mots = "thislinewaswritten"
+console.log(mots.length);
+
+let text = "We are the so-called \"Vikings\" from \n the north.";
+console.log(text);
+document.getElementById("Pstrings").innerHTML = text ;
+
+let x = "john";
+let y = new String("john");
+console.log(typeof x);
+console.log(typeof y);
+console.log(x==y);
