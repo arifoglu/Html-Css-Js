31927b31c67ab1161c9dbb9066991e5d330b5d06
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-09-01 16:15:13 +0200

    i try different things with js

---
+++ js_activite/trying/try1.js
@@ -0,0 +1,9 @@
+function myFunction(p1){   
+    if (p1>=18) {
+        console.log("this person can buy somethings")
+    } else {
+        console.log("this person CAN NOT buy somethings")
+    }
+    return
+};
+myFunction(18);
