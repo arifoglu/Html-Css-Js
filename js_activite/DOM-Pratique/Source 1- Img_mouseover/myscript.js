b7230a24e0cee82e643c7dba277d0bf70a676be6
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-09-13 13:34:28 +0200

    i work with js DOM

--- js_activite/DOM-Pratique/Source 1- Img_mouseover/myscript.js
+++ js_activite/DOM-Pratique/Source 1- Img_mouseover/myscript.js
@@ -1,10 +1,32 @@
+/////  onmouseover()    and this
 document.querySelector("img.blue").onmouseover = function()
 {
-    this.src = "images/green.jpg";
-    document.querySelector("div#container").style.backgroundColor = "green";
+    this.src = "/DOM-Pratique/Source 1- Img_mouseover/images/green.jpg";
+    document.querySelector("div#container").style.backgroundColor = "#073d41" ;
 };
+///// onmouseover()     and  this
 document.querySelector("img.blue").onmouseout = function()
 {
-    this.src = "images/blue.jpg";
-    document.querySelector("div#container").style.backgroundColor = "#faebd7"
+    this.src = "/DOM-Pratique/Source 1- Img_mouseover/images/blue.jpg";
+    document.querySelector("div#container").style.backgroundColor = "#719171" ;
 };
+
+let images =document.querySelectorAll("div#container > img");
+// console.log(images);
+
+Array.prototype.forEach.call(images,function(imgObjet){
+//    console.log(imgObjet);
+
+    imgObjet.onclick = function(){
+        this.style.opacity = "0.5";
+        this.classList.add("anim");
+        console.log(imgObjet)
+    };
+    imgObjet.ondblclick = function(){
+        this.style.opacity = 1;
+        this.classList.remove("anim");
+        console.log(imgObjet);
+    };
+});
+
+
