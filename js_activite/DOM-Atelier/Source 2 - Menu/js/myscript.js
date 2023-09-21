730bf7c81748e695e7edc61a46fe7b451f197d3e
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-09-21 11:00:35 +0200

    i added some codes dom atelier file

--- js_activite/DOM-Atelier/Source 2 - Menu/js/myscript.js
+++ js_activite/DOM-Atelier/Source 2 - Menu/js/myscript.js
@@ -1,26 +1,23 @@
 window.onload = function(){
+ let rubriques = document.querySelectorAll("ul#navigation > li ");
 
-let rubriques = document.querySelectorAll("ul#navigation > li");
-// console.log(rubriques);   NodeList(3) [ li, li, li ]
+ console.log(rubriques); // NodeList(3) [ li, li, li ]
 
-Array.prototype.forEach.call(rubriques,function(liObj){
-// console.log(liObj);      li li li 
+ Array.prototype.forEach.call(rubriques,function( liObj ){
+  
+     liObj.querySelector("a").onclick = function(e){
+      e.preventDefault();
+     };
 
-          liObj.querySelector("a").onclick = function (e)
-          {
-            e.preventDefault();
-            console.log(e);
-          }  ;
+  if(liObj.querySelector("ul.sousmenu"))
+   
+       liObj.onmouseover = function(){
+       liObj.querySelector("ul.sousmenu").style.display = "block";
+     }
+       liObj.onmouseout = function(){
+       liObj.querySelector("ul.sousmenu").style.display = "none";
+  };
 
-          if(liObj.querySelector("ul.sousmenu"))
+ });
 
-          liObj.onmouseover = function(){
-            liObj.querySelector("ul.sousmenu").style.display = "block";
-          }
-          liObj.onmouseout = function(){
-            liObj.querySelector("ul.sousmenu").style.display = "none";
-          }
-
-
-})
-}
+};
