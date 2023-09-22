8bcd33147b9eb3511a353826e15444121abad863
arifoglu <88225121+arifoglu@users.noreply.github.com>
2023-09-22 10:32:00 +0200

    i changed directory

--- js_activite/js-practice/practice4.js
+++ js_activite/js-practice/practice4.js
@@ -12,7 +12,7 @@ window.onload = function(){
         c =Number(document.getElementById("numero3").value);
         // console.log(c);
         console.log(typeof a); // string
-
+        let resultat = document.getElementById("result");
        /// this function can write if you want but "" Number""" method more than efficace
        // function myfunction(){
        //            a =Number(a) ;
@@ -25,31 +25,31 @@ window.onload = function(){
         
            //comparison options//
     if(a > b && a > c && b > c)
-    document.getElementById("result").innerHTML = a + ">" + b + ">" + c ;
+    resultat.innerHTML = a + ">" + b + ">" + c ;
     else if(a > b && a > c && c > b)
-    document.getElementById("result").innerHTML = a + ">" + c + ">" + b ;
+    resultat.innerHTML = a + ">" + c + ">" + b ;
     else if(b > c && b > a && c > a)
-    document.getElementById("result").innerHTML = b + ">" + c + ">" + a ;
+    resultat.innerHTML = b + ">" + c + ">" + a ;
     else if(b > a && b > c && a > c)
-    document.getElementById("result").innerHTML = b + ">" + a + ">" + c ;
+    resultat.innerHTML = b + ">" + a + ">" + c ;
     else if(c > a && c > b && a > b)
-    document.getElementById("result").innerHTML = c + ">" + a + ">" + b ;
+    resultat.innerHTML = c + ">" + a + ">" + b ;
     else if(c > a && c > b && b > a)
-    document.getElementById("result").innerHTML = c + ">" + b + ">" + a ;
+    resultat.innerHTML = c + ">" + b + ">" + a ;
     else if(a == b && a < c )
-    document.getElementById("result").innerHTML = a + "=" + b + "<" + c ;
+    resultat.innerHTML = a + "=" + b + "<" + c ;
     else if(a == b && a > c )
-    document.getElementById("result").innerHTML = a + "=" + b + ">" + c ;
+    resultat.innerHTML = a + "=" + b + ">" + c ;
     else if(b == c && a > c )
-    document.getElementById("result").innerHTML = a + ">" + b + "=" + c ;
+    resultat.innerHTML = a + ">" + b + "=" + c ;
     else if(b == c && a < c )
-    document.getElementById("result").innerHTML = a + "<" + b + "=" + c ;
+    resultat.innerHTML = a + "<" + b + "=" + c ;
     else if(a == c && a > b )
-    document.getElementById("result").innerHTML = a + "=" + c + ">" + b ;
+    resultat.innerHTML = a + "=" + c + ">" + b ;
     else if(a == c && a < b )
-    document.getElementById("result").innerHTML = a + "=" + c + "<" + b ;
+    resultat.innerHTML = a + "=" + c + "<" + b ;
     else
-    document.getElementById("result").innerHTML = "EGAL"
+    resultat.innerHTML = "EGAL"
     };
    /// 
     
@@ -71,7 +71,10 @@ window.onload = function(){
    // x.forEach(function(p){
    //  p.value = "";
    // });
-   //  option 2.1
+
+
+   ////      option 2.1
+   
    document.querySelectorAll("div#container > input").forEach((p)=>{ p.value = ""});
    ;
 
